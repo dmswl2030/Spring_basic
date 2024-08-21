0cef3c3c1f9afb5a72c2fc826b06b6e0ade54dd8
EUNJI <eunji9180@naver.com>
2024-08-21 23:12:57 +0900

    새로운 할인 정책 적용과 문제점

--- src/main/java/hello/SpringBasic/order/OrderServiceImpl.java
+++ src/main/java/hello/SpringBasic/order/OrderServiceImpl.java
@@ -1,14 +1,14 @@
 package hello.SpringBasic.order;
 
-import hello.SpringBasic.discount.FixDiscountPolicy;
+import hello.SpringBasic.discount.DiscountPolicy;
 import hello.SpringBasic.member.Member;
 import hello.SpringBasic.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
     private final MemoryMemberRepository memberRepository = new MemoryMemberRepository();
-    private final FixDiscountPolicy discountPolicy = new FixDiscountPolicy();
-
+    private DiscountPolicy discountPolicy; //구체에 의존하지 않고 추상화인 인터페이스에만 의존한다
+    
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
